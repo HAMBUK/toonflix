d0b6b9b23638b5125ea2c164885004f70e6d29ec
HAMBUK <108109292+HAMBUK@users.noreply.github.com>
2023-11-14 22:57:39 +0900

    flutter_for_beginners

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
+class Player {
+  String? name;
+  Player({required this.name});
+}
+
 void main() {
+  var nico = Player(name: "Potato");
   runApp(App());
 }
 
@@ -10,9 +16,12 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          centerTitle: false,
           title: Text('Hello flutter!'),
         ),
-        body: Center(child: Text('Hello world!')),
+        body: Center(
+          child: Text('Hello world!'),
+        ),
       ),
     ); // Material 구글, Cupertino ios의 root widget
   }
